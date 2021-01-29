256be51ddb6074c1ea9d44214f1a66d7afab0a72
Triston Herbst <tristonherbst97@gmail.com>
2021-01-29 21:02:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   num_comp = 1000000000000000000000000000000000000000000000000000000
-  key_store = ""
+  key_store = nil
   name_hash.each do |key, value|
     if value < num_comp
       key_store = key
